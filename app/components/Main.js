28dd3216c06cf062fc0605539806c674655865bd
nawatend <32384185+nawatend@users.noreply.github.com>
2019-03-12 16:49:36 +0100

    new screens

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Button } from 'react-native'
 import { createStackNavigator, createAppContainer } from 'react-navigation'; // Version can be specified in package.json
 import CardContainer from './CardContainer'
 import { Title, SubTitle } from './textComponents/'
 import { backGradient } from '../utils/styles/Colors'
 import Navigation from './Navigation'
+import NavigationItem from './NavigationItem'
+
 import { LinearGradient } from 'expo'
 import TaskContainer from './TaskContainer'
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -20,24 +23,34 @@ const styles = StyleSheet.create({
     paddingLeft: 20,
     paddingRight: 20,
     height: 'auto',
+  },
+  test: {
+    color: 'yellow',
   }
 });
 
 class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
+      <LinearGradient colors={backGradient} style={styles.container}>
+        {/* Insert top text here */}
+        <View style={styles.textContainer}>
+          <Title style={styles.test} text={'Welcome, User'} />
+          <SubTitle text={'How are you feeling today?'} />
+
+        </View>
         <Button
           title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
+          onPress={() => this.props.navigation.push('Details')}
         />
-      </View>
+        <TaskContainer />
+        <Navigation route={() => this.props.navigation.push('Details')} />
+      </LinearGradient>
     );
   }
 }
 
-export class Main extends Component {
+class TaskScreen extends React.Component {
   render() {
     return (
       <LinearGradient colors={backGradient} style={styles.container}>
@@ -47,12 +60,58 @@ export class Main extends Component {
           <SubTitle text={'How are you feeling today?'} />
 
         </View>
-        <TaskContainer />
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.push('Details')}
+        />
+        <CardContainer />
         <Navigation />
-
       </LinearGradient>
+    );
+  }
+}
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: TaskScreen,
+  },
+  {
+    initialRouteName: 'Home',
+    headerMode: 'none',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export class Main extends Component {
+  // render() {
+  //   return (
+  //     <LinearGradient colors={backGradient} style={styles.container}>
+  //       {/* Insert top text here */}
+  //       <View style={styles.textContainer}>
+  //         <Title text={'Welcome, User'} />
+  //         <SubTitle text={'How are you feeling today?'} />
+
+  //       </View>
+  //       <Button
+  //         title="Go to Details"
+  //         onPress={() => this.props.navigation.navigate('Details')}
+  //       />
+  //       <TaskContainer />
+  //       <Navigation />
+
+  //     </LinearGradient>
+
+  //   )
+  // }
+
+  render() {
+    return (
+      <AppContainer />
+
 
     )
+
   }
 }
 
--- app/utils/styles/Colors.js
+++ app/utils/styles/Colors.js
@@ -1,14 +1,19 @@
+//DarkMode
 export const darkPurple = '#1e0060'
 export const midPurple = '#332767'
+// Gradients
+export const backGradient = [midPurple, darkPurple]
 
+
+
+//emote/task gradients
 export const exellentCardGradient = ['#65F5BD', '#58FF5F']
 export const okayCardGradient = ['#4091FF', '#0050FF']
 export const stressGradient = ['#FFBD40', '#FFEB00']
 export const anxiousGradient = ['#F50097', '#FF34EF']
 export const exhaustedGradient = ['#0009F5', '#8388ff']
 
-// Gradients
-export const backGradient = [midPurple, darkPurple]
 
+//text colors and highlights
 export const mainTextColor = '#FFFFFF'
 export const highLight = '#F50097'
