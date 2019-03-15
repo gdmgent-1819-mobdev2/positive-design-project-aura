50ff1f42eb2c8129fb9cc3d5bc3100ad9a4a43d7
nawatend <32384185+nawatend@users.noreply.github.com>
2019-03-15 22:05:33 +0100

    day statistic

--- app/components/statistic/Day.js
+++ app/components/statistic/Day.js
@@ -1,23 +1,52 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { LinearGradient } from 'expo'
+import {
+    exellentCardGradient, okayCardGradient, stressGradient,
+    anxiousGradient, exhaustedGradient
+} from '../../utils/styles'
+
 
 const styles = StyleSheet.create({
 
     day: {
-        width: 10,
+        width: 20,
         borderWidth: 1,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+        backgroundColor: 'white',
 
     }
 
 })
 
-const Day = (average) => (
+/**
+ * 
+ * @param {averageEmotion} Number from 1 to 10
+ */
+const Day = ({ averageEmotion }) => {
+    let gradientColor = [];
+    if (averageEmotion < 40) {
+        gradientColor = exhaustedGradient;
+    } else if (averageEmotion > 40 && averageEmotion <= 80) {
+        gradientColor = anxiousGradient
+    } else if (averageEmotion > 80 && averageEmotion <= 120) {
+        gradientColor = stressGradient
+    } else if (averageEmotion > 120 && averageEmotion <= 160) {
+        gradientColor = okayCardGradient
+    } else if (averageEmotion > 160 && averageEmotion <= 200) {
+        gradientColor = exellentCardGradient
+    }
 
-    <View style={styles.day}>
+    return (
+        <LinearGradient style={[{ height: averageEmotion }, styles.day]} colors={gradientColor}>
 
-    </View>
+        </LinearGradient>
 
+    )
 
-)
+}
 
 export default Day
---
+++ app/components/statistic/Week.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { LinearGradient } from 'expo'
+import {
+    exellentCardGradient, okayCardGradient, stressGradient,
+    anxiousGradient, exhaustedGradient
+} from '../../utils/styles'
+
+
+const styles = StyleSheet.create({
+
+    week: {
+        width: 40,
+        borderWidth: 1,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+        backgroundColor: 'white',
+
+    }
+
+})
+
+/**
+ * 
+ * @param {averageEmotion} Number from 0 to 200
+ */
+const Week = ({ averageEmotion }) => {
+    let gradientColor = [];
+    if (averageEmotion < 40) {
+        gradientColor = exhaustedGradient;
+    } else if (averageEmotion > 40 && averageEmotion <= 80) {
+        gradientColor = anxiousGradient
+    } else if (averageEmotion > 80 && averageEmotion <= 120) {
+        gradientColor = stressGradient
+    } else if (averageEmotion > 120 && averageEmotion <= 160) {
+        gradientColor = okayCardGradient
+    } else if (averageEmotion > 160 && averageEmotion <= 200) {
+        gradientColor = exellentCardGradient
+    }
+
+    return (
+        <LinearGradient style={[{ height: averageEmotion }, styles.week]} colors={gradientColor}>
+
+        </LinearGradient>
+
+    )
+
+}
+
+export default Day
