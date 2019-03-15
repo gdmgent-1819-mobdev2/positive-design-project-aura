fcb211e1ae600db714fce99078459e249c6d76b5
Bruno De Coene <brunodecoene@Brunos-MacBook-Pro-2.local>
2019-03-15 17:48:12 +0100

    Icons added to TaskCards

--- app/components/TaskCard.js
+++ app/components/TaskCard.js
@@ -1,37 +1,45 @@
-import React, { Component } from 'react'
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
-import { LinearGradient } from 'expo'
-import { highLight, mainTextColor, stressGradient } from '../utils/styles'
+import React, { Component } from "react";
+import { Text, View, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { LinearGradient } from "expo";
+import { highLight, mainTextColor, stressGradient } from "../utils/styles";
 
 const styles = StyleSheet.create({
-    taskcard: {
-        width: 140,
-        height: 140,
-        //backgroundColor: '#48efbe',
-        borderRadius: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-        margin: 5,
-    },
-    container: {
-        width: 140,
-        height: 140,
-        borderRadius: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-        margin: 5,
-    }
-})
+  taskcard: {
+    width: 140,
+    height: 140,
+    //backgroundColor: '#48efbe',
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    margin: 5
+  },
+  container: {
+    width: 140,
+    height: 140,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 5
+  },
+  icon: {
+    width: 50,
+    height: 50
+  }
+});
 
-const TaskCard = ({ text, gradient }) => (
-    <TouchableOpacity>
-        <LinearGradient colors={gradient} style={styles.container}>
-            <View style={styles.taskcard}>
-                <Text>{text}</Text>
-            </View>
-        </LinearGradient>
-    </TouchableOpacity>
-)
+const TaskCard = ({ text, gradient, image }) => (
+  <TouchableOpacity>
+    <LinearGradient colors={gradient} style={styles.container}>
+      <View style={styles.taskcard}>
+        <Image
+          source={image}
+          fadeDuration={0}
+          style={styles.icon}
+        />
+      </View>
+    </LinearGradient>
+  </TouchableOpacity>
+);
 
-export default TaskCard
+export default TaskCard;
