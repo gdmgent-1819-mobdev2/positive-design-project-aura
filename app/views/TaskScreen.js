a448dc9d081a0e99189e4350b7a7c99ae6fc7621
nawatend <32384185+nawatend@users.noreply.github.com>
2019-03-16 14:56:23 +0100

    Update TaskScreen.js

--- app/views/TaskScreen.js
+++ app/views/TaskScreen.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Text, View, StyleSheet, Button } from 'react-native'
 import { LinearGradient } from 'expo'
 import TaskCard from '../components/TaskCard'
-import {exellentCardGradient, okayCardGradient, stressGradient, anxiousGradient, exhaustedGradient} from '../utils/styles'
+import { exellentCardGradient, okayCardGradient, stressGradient, anxiousGradient, exhaustedGradient } from '../utils/styles'
 import Navigation from '../components/Navigation'
 import { Title, SubTitle } from '../components/textComponents/'
 import { ArrowButton } from '../components/buttonComponents/'
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     alignItems: 'center',
     justifyContent: 'space-between'
-},
+  },
 });
 
 class TaskScreen extends Component {
@@ -66,14 +66,14 @@ class TaskScreen extends Component {
           <Navigation route={() => this.props.navigation.push('Home')} />
         </View> */}
         <View style={styles.textContainer}>
-        <ArrowButton />
+          <ArrowButton />
           <SubTitle text={'Now we know how you feel, let\'s do something!'} />
         </View>
         <View style={styles.taskContainer}>
-        <TaskCard text='Excercise' gradient={okayCardGradient} />
-        <TaskCard text='Mind' gradient={stressGradient} />
-        <TaskCard text='Chat' gradient={exellentCardGradient} />
-        <TaskCard text='Quote' gradient={anxiousGradient} />
+          <TaskCard text='Excercise' gradient={okayCardGradient} />
+          <TaskCard text='Mind' gradient={stressGradient} />
+          <TaskCard text='Chat' gradient={exellentCardGradient} />
+          <TaskCard text='Quote' gradient={anxiousGradient} />
         </View>
       </LinearGradient>
     );
