9436cd3fb9020f7681a18e54bb3a5fecf5ed2d27
nawatend <32384185+nawatend@users.noreply.github.com>
2019-03-24 00:22:53 +0100

    Update HomeScreen.js

--- app/views/HomeScreen.js
+++ app/views/HomeScreen.js
@@ -113,7 +113,7 @@ class HomeScreen extends Component {
         this.setState({
           loading: false,
           allowEmotion: false,
-          emoteTimer: hours + ":" + minutes + ":" + seconds,
+          emoteTimer: ("0" + hours).slice(-2) + ":" + ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2),
         })
       } else {
         this.setState({
@@ -121,13 +121,6 @@ class HomeScreen extends Component {
           allowEmotion: true,
         })
       }
-
-
-
-
-
-
-
 
     } else {
       this.setState({
@@ -194,7 +187,7 @@ class HomeScreen extends Component {
         return (
           <LinearGradient colors={backGradient} style={styles.container}>
             <View style={styles.textContainer}>
-              <Title text={'Welcome, ' + this.state.currentUserName} />
+              <Title text={'Hello, ' + this.state.currentUserName} />
               <SubTitle text={'How are you feeling today?'} />
 
             </View>
