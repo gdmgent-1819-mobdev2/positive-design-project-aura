92283e85d4d78ad108b36445b9a8a8874e986347
Bruno De Coene <brunodecoene@Brunos-MacBook-Pro-2.local>
2019-03-23 19:38:07 +0100

    Email and password validation

--- app/views/Register/RegisterBase.js
+++ app/views/Register/RegisterBase.js
@@ -1,8 +1,20 @@
-import React, { Component } from 'react'
-import { Text, View, StyleSheet, TextInput, TouchableOpacity, Alert } from 'react-native'
-import { SubTitle, SecondarySubtitle, Logo, Body } from '../../components/textComponents';
-import { SecondaryButton } from '../../components/buttonComponents';
-import { getInstance } from '../../services/firebase/firebase'
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Alert
+} from "react-native";
+import {
+  SubTitle,
+  SecondarySubtitle,
+  Logo,
+  Body
+} from "../../components/textComponents";
+import { SecondaryButton } from "../../components/buttonComponents";
+import { getInstance } from "../../services/firebase/firebase";
 
 const styles = StyleSheet.create({
   container: {
@@ -15,107 +27,130 @@ const styles = StyleSheet.create({
     backgroundColor: "#2B1576"
   },
   horizontalButtons: {
-    width: '100%',
+    width: "100%",
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between"
   },
   inputField: {
-    backgroundColor: '#ffffff',
+    backgroundColor: "#ffffff",
     opacity: 0.9,
     height: 40,
-    width: '100%',
+    width: "100%",
     borderRadius: 20,
     paddingLeft: 10,
     marginTop: 10
   },
   fullWidth: {
-    width: '100%',
+    width: "100%"
   },
   primaryBtn: {
     fontSize: 24,
-    fontWeight: '400',
-    backgroundColor: '#F50097',
+    fontWeight: "400",
+    backgroundColor: "#F50097",
     minWidth: 180,
     paddingTop: 12,
     paddingBottom: 12,
     borderRadius: 22,
-    marginTop: 20,
+    marginTop: 20
   },
   btnText: {
     fontSize: 18,
-    fontWeight: '400',
-    color: '#ffffff',
-    textAlign: 'center',  
-  },
-})
+    fontWeight: "400",
+    color: "#ffffff",
+    textAlign: "center"
+  }
+});
 
-const firebase = getInstance()
+const firebase = getInstance();
 
 export class RegisterBase extends Component {
-  
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      email: '',
-      password: '',
-      pass2: '',
-    }
+      email: "",
+      password: "",
+      pass2: ""
+    };
   }
 
   createUser = () => {
     const userdata = {
       email: this.state.email,
       password: this.state.password,
-      pass2: this.state.pass2,
-    }
+      pass2: this.state.pass2
+    };
 
-    if(userdata.password === userdata.pass2) {
-      this.props.navigation.navigate('Person', { data: userdata})
+    if (userdata.email != "") {
+      if (userdata.password == "" || userdata.pass2 == "") {
+        Alert.alert("One or more fields are empty, please check all fields!");
+      } else {
+        if (userdata.password === userdata.pass2) {
+          this.props.navigation.navigate("Person", { data: userdata });
+        } else {
+          Alert.alert("Passwords do not match!");
+        }
+      }
+    } else {
+      Alert.alert("Please choose a correct email adress");
     }
-    else {
-      Alert.alert('Passwords do not match!')
-    }
-  }
-  
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <View>
           <Logo />
-          <SecondarySubtitle text={'As a first stap towards your account, we will need your email address and password'} />
+          <SecondarySubtitle
+            text={
+              "As a first stap towards your account, we will need your email address and password"
+            }
+          />
         </View>
         <View style={styles.fullWidth}>
-          <TextInput 
-            style={styles.inputField} 
-            onChangeText={(text) => {this.setState({email: text})}}
-            placeholder={'E-mail'}
-            keyboardType={'email-address'}
+          <TextInput
+            style={styles.inputField}
+            onChangeText={text => {
+              this.setState({ email: text });
+            }}
+            placeholder={"E-mail"}
+            keyboardType={"email-address"}
           />
-          <TextInput 
-            style={styles.inputField} 
-            onChangeText={(text) => {this.setState({password: text})}}
-            placeholder={'Password'}
+          <TextInput
+            style={styles.inputField}
+            onChangeText={text => {
+              this.setState({ password: text });
+            }}
+            placeholder={"Password"}
             secureTextEntry={true}
           />
-          <TextInput 
+          <TextInput
             style={styles.inputField}
-            onChangeText={(text) => {this.setState({pass2: text})}} 
-            placeholder={'Confirm Password'}
+            onChangeText={text => {
+              this.setState({ pass2: text });
+            }}
+            placeholder={"Confirm Password"}
             secureTextEntry={true}
           />
         </View>
         <View>
-          <SecondaryButton text={'Cancel'} route={'Login'} navigation={this.props.navigation.navigate} />
-          <TouchableOpacity style={styles.primaryBtn} onPress={() => {this.createUser()}}>
-            <Text style={styles.btnText}>
-              Next
-            </Text>
+          <SecondaryButton
+            text={"Cancel"}
+            route={"Login"}
+            navigation={this.props.navigation.navigate}
+          />
+          <TouchableOpacity
+            style={styles.primaryBtn}
+            onPress={() => {
+              this.createUser();
+            }}
+          >
+            <Text style={styles.btnText}>Next</Text>
           </TouchableOpacity>
         </View>
       </View>
-    )
+    );
   }
 }
 
-export default RegisterBase
+export default RegisterBase;
